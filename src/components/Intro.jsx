a8778aa661eb5b5f75e62cc7889bc0f113deeea5
Pelayo Trives <pelayotrives@gmail.com>
2022-06-27 11:34:03 +0200

    Commit inicial.

---
+++ src/components/Intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div>
+        <div className="intro-left">Left aside</div>
+        <div className="intro-right">Right aside</div>
+    </div>
+  )
+}
+
+export default Intro
